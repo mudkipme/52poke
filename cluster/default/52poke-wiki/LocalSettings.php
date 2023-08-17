591e26f75b3a68ad261612d7143bde6e1bd01903
Mudkip <i@mudkip.me>
2023-08-18 01:50:20 +0800

    Display sidebar by default

--- cluster/default/52poke-wiki/LocalSettings.php
+++ cluster/default/52poke-wiki/LocalSettings.php
@@ -172,6 +172,8 @@ $wgVectorMaxWidthOptions = [
 ];
 $wgVectorLanguageInMainPageHeader = true;
 $wgVectorTableOfContentsCollapseAtCount = 200;
+$wgVectorDefaultSidebarVisibleForAuthorisedUser = true;
+$wgVectorDefaultSidebarVisibleForAnonymousUser = true;
 
 ## For attaching licensing metadata to pages, and displaying an
 ## appropriate copyright notice / icon. GNU Free Documentation
